0bdd413f7c954c03c2f261efacb2c19b8151792f
Benny Kotler <bkotler@umd.edu>
2022-04-15 14:06:36 -0400

    Final commit for check in

--- final_project.py
+++ final_project.py
@@ -46,7 +46,18 @@ class Game:
         Returns:
              str: Will return the guesses made by the computer (letters or a word)
         """
-       
+        
+    def show_screen(self, player, computer, points, length, guess_number):
+        """ This function displays the screen of the game to the player. This includes the players score,
+        the computers score, the length of the word being guessed as well as its clue, and the amount of
+        guess the player and computer have used respectivly. 
+        Args:
+            player (str): The players name stored as a string.
+            computer (str): The computer and its difficulty stored as a string.
+            points (int): The points that the player or computer have stored as an integer.
+            length (int): The length of the word stored as an integer.
+            guess_number(int): The amount of guesses the user or computer have used stored as an integer.
+        """
         
     def calculate_points(self, player, computer, points, length, guess_number):
         """ This function calculates the players and computers points respectivly. It uses the amount of guesses
