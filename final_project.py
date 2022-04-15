a9b87e7278f232288fa20ee6216166e8c83bfb46
Jaron Richman <jhrichman@gmail.com>
2022-04-14 21:02:04 -0400

    skeleton

---
+++ final_project.py
@@ -0,0 +1,26 @@
+class Game:
+    """Docstring
+    """
+    
+    def __init__():
+        
+    
+    def open_files():
+        
+    
+    def user_guesses():
+        
+        
+    def computer_guesses():
+        
+        
+    def show_screen():
+        
+        
+    def calculate_points():
+        
+        
+    def pick_opponent():
+        
+        
+    def pick_difficulty():
