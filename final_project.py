b485ec06c8683a8e799d2770676d91e705d568fe
Sorian Kamara <kamarasorian@gmail.com>
2022-04-15 13:55:32 -0400

    I added user and computer guesses

--- final_project.py
+++ final_project.py
@@ -8,10 +8,28 @@ class Game:
     def open_files():
         
     
-    def user_guesses():
+    def user_guesses(self, player, guess_length):
+        """This function will take the letters or word guessed by the player and it will return a match stored in the file.
+
+        Args:
+            player (str): The player will identify who they are
+            guess_length (str):The length of the word guessed by the player
+        Returns:
+             str: Will return the guesses made by the player (letters or a word)
+        """
+      
         
         
-    def computer_guesses():
+    def computer_guesses(self, player, guess_length):
+        """This function will take the letters or word guessed by the player and it will return a match stored in the file.
+
+        Args:
+            player (str): The computer will let the humna player know they are playing against the computer
+            guess_length (str): The length of the word guessed by the computer player
+        Returns:
+             str: Will return the guesses made by the computer (letters or a word)
+        """
+       
         
         
     def show_screen():
