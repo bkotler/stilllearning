325a97f7d9e3aa59d3132cba1edbe7cb14b99b5c
Jaron Richman <jhrichman@gmail.com>
2022-04-15 10:52:32 -0400

    My part

--- final_project.py
+++ final_project.py
@@ -20,7 +20,18 @@ class Game:
     def calculate_points():
         
         
-    def pick_opponent():
+    def pick_opponent(self, user, name):
+        '''This function picks whether the user is playing against the computer or another person
+        
+            Args:
+                user (str): the type of opponent
+                name (str): the name of the opponent
+        '''
         
         
-    def pick_difficulty():
+    def pick_difficulty(self, difficulty):
+        '''This function will determine the difficulty of the game, ranging from easiest to hardest
+        
+            Args:
+                difficulty (str): the level of difficulty
+        '''
