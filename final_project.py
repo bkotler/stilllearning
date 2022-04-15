9b6fedc2cc64aeb54ad418e534473abc6baab78e
Benny Kotler <bkotler@umd.edu>
2022-04-15 12:31:37 -0400

    Benny's part

--- final_project.py
+++ final_project.py
@@ -2,11 +2,24 @@ class Game:
     """Docstring
     """
     
-    def __init__():
-        
+    def __init__(player_name):
+    """This will set up the game and initiate the game attributes
+    Args:
+        player_name (str): The name of the player
+    Side effects:
+        Sets all the attributes and initilizes the game
+    """
     
-    def open_files():
-        
+    def open_files(words_path, clues_path):
+        """This will open two files and set them to lists. One file will be
+            for the words and another for the clues.
+        Args:
+            words_path (str): Filename of the words file
+            clues_path (str): Filename of the clues file
+        Side effects:
+            Sets the words_list and clues_list attributes to the data from the
+                files.
+        """
     
     def user_guesses():
         
@@ -24,3 +37,6 @@ class Game:
         
         
     def pick_difficulty():
+        
+    if __name__ == "__main__":
+        new_game = Game(player_name)
